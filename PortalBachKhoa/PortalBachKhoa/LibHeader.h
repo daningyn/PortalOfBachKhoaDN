a8e68d0b30578c80c75026ad6e6de8af972de3eb
LisM <danh12t4@gmail.com>
2016-07-12 10:50:41 +0700

    + Add Header to connect library objective-C

---
+++ PortalBachKhoa/PortalBachKhoa/LibHeader.h
@@ -0,0 +1,17 @@
+//
+//  LibHeader.h
+//  PortalBachKhoa
+//
+//  Created by Danh Nguyen on 7/1/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+#ifndef LibHeader_h
+#define LibHeader_h
+
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+
+#endif /* LibHeader_h */
