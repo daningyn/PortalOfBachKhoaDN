66c12135cbfc843524dd53a82a29ba3161fed0a6
LisM <danh12t4@gmail.com>
2016-07-12 11:52:15 +0700

    + Create ViewController save title news on datao

---
+++ PortalBachKhoa/PortalBachKhoa/TableViewCell/TitleCell.swift
@@ -0,0 +1,27 @@
+//
+//  TitleCell.swift
+//  PortalBachKhoa
+//
+//  Created by Danh Nguyen on 7/2/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class TitleCell: UITableViewCell {
+
+    @IBOutlet weak var titleDate: UILabel!
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
