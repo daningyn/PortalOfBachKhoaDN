6c4c744cf2f6dcb326e58800e1d64c8cbe32eafb
LisM <danh12t4@gmail.com>
2016-07-12 10:45:30 +0700

    + Create Library Image + Create UI + Create Podfile

---
+++ PortalBachKhoa/PortalBachKhoa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PortalBachKhoa
+//
+//  Created by Danh Nguyen on 7/1/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
